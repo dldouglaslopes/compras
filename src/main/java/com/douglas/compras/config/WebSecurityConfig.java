aff0373e18f344f0be5db50ed4ff3b8b20375908
douglas <Douglas Lopes Dias>
2018-08-15 18:48:50 -0600

    Novo login e novo index

--- src/main/java/com/douglas/compras/config/WebSecurityConfig.java
+++ src/main/java/com/douglas/compras/config/WebSecurityConfig.java
@@ -25,7 +25,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	public static final String[] PUBLIC_MATCHERS = {
 			"/",
-			"/h2-console**/"
+			"/h2-console**/",
+			"/login"
 	};
 	
 	public static final String[] PUBLIC_MATCHERS_GET = {
