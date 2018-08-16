10b98a28f9e6148b3b20d79a7aa2261502e6a90c
Douglas Lopes Dias <dldouglaslopes@gmail.com>
2018-08-16 17:22:56 -0300

    Autenticacao e Autorizacao

--- src/main/java/com/douglas/compras/config/WebSecurityConfig.java
+++ src/main/java/com/douglas/compras/config/WebSecurityConfig.java
@@ -7,14 +7,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.douglas.compras.security.JWTUtil;
+import com.douglas.compras.security.JwtAuthenticationFilter;
+import com.douglas.compras.security.JwtAuthorizationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +28,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private Environment environment;
+	@Autowired
+	private UserDetailsService userDetailsService;
+	@Autowired
+	private JWTUtil jwtUtil;
 	
 	public static final String[] PUBLIC_MATCHERS = {
 			"/",
@@ -32,8 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public static final String[] PUBLIC_MATCHERS_GET = {
 			"/produtos**/",
 			"/*categorias*/"
-	};
-	
+	};	
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -46,7 +55,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.GET ,PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
+		
+		http.addFilter(new JwtAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JwtAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
+
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
 	
 	@Bean
